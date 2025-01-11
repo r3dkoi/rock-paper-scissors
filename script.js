51441411a11a5096ec61cf59d3ffdf540949b46d
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-01-12 07:45:05 +0800

    fixed the () to [] in line 8 because () is for calling functions [] is for accessing arrays.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function playGame() {
+// function playGame() {
     humanScore = 0
     computerScore = 0
 
     //Computer Choice function
     function getComputerChoice() {
         const computerChoices = ["rock", "paper", "scissors"];
-        const computerChoice = computerChoices(Math.floor(Math.random() * computerChoices.length)); 
+        const computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
             console.log(`Computer chose ${computerChoice}`);
             return computerChoice;
         
     }
-}
+// } 
